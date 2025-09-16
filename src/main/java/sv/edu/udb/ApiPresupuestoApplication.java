0308f64e6c1cb883560b2795aa907462b9089010
Daniel <dfquack08@gmail.com>
2025-09-16 12:36:54 -0600

    Initial commit

---
+++ src/main/java/sv/edu/udb/ApiPresupuestoApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPresupuestoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPresupuestoApplication.class, args);
+    }
+
+}
